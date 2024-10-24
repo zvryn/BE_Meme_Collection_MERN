df9f40224e261bfc9a66f327236aecbd9d681b33
zvryn <sarinkanok@gmail.com>
2024-10-24 14:45:16 +0700

    add api for update post

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ import postRoutes from "./routes/post.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://jackoff:jackoff1234@cluster0.s4ntd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
